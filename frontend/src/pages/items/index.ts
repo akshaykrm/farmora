ae0ae226d7d84e8a9a9a9966a1f3a1c854e657ee
Akshay Krishna <akshaykm@fastmail.com>
2026-09-26 12:27:51 +0530

    fix(items): uncommented medicine option

--- frontend/src/pages/items/index.ts
+++ frontend/src/pages/items/index.ts
@@ -6,7 +6,7 @@ export { api as itemsApi };
 export const itemTypes = [
   { label: "Regular", value: "regular" },
   { label: "Chick", value: "chick" },
-  // { label: "Medicine", value: "medicine" },
+  { label: "Medicine", value: "medicine" },
   { label: "FINISHER", value: "FINISHER" },
   { label: "STARTER", value: "STARTER" },
   { label: "PRE STARTER", value: "PRE STARTER" },
